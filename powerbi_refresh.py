87028f9416fe8827e42c2d8e767560ea677b63fd
ChoKaPeek <ChoKaPeek@users.noreply.github.com>
2021-01-28 13:51:56 +0100

    doc: better doc and logs

--- powerbi_refresh.py
+++ powerbi_refresh.py
@@ -10,6 +10,7 @@ from pywinauto import timings
 PROCNAME = "PBIDesktop.exe"
 REFRESH_INTERVAL = 60*2
 FORMAT = "%(asctime)-15s %(message)s"
+TIMEOUT = 60
 
 
 def type_keys(string, element):
@@ -25,7 +26,7 @@ def main():
   parser.add_argument("excel", help = "Path to .xlsm file")
   args = parser.parse_args()
 
-  timings.after_clickinput_wait = 1
+  timings.after_clickinput_wait = 1  # Default 0.09
 
   for proc in psutil.process_iter():
     if proc.name() == PROCNAME:
@@ -33,37 +34,39 @@ def main():
       return False
 
   # Start PBI and open the workbook
-  logging.info("\n")
-  logging.info("Starting Power BI")
+  logging.info("")
+  logging.info("Power BI Starting...")
   os.system('start "" "' + args.workbook + '"')
   time.sleep(5)
 
   # Connect pywinauto
-  logging.info("Identifying Power BI window")
+  logging.info("Connecting to Power BI...")
   app = Application(backend='uia').connect(path=PROCNAME)
   win = app.window(title_re='.*Power BI Desktop')
-  win.wait("ready", timeout=30)
+  win.wait("ready", timeout=TIMEOUT)
   win.Save.click()  # Save the window
-  win.wait("enabled", timeout=30)
+  win.wait("enabled", timeout=TIMEOUT)
   win.set_focus()  # Make window visible
-  win.wait("enabled", timeout=30)
+  win.wait("enabled", timeout=TIMEOUT)
   win.maximize()  # Put window in fullscreen 
-  win.wait("enabled", timeout=30)
+  win.wait("enabled", timeout=TIMEOUT)
   win.Visualisations.click()  # Close Visualisations toolbox
-  win.wait("enabled", timeout=30)
+  win.wait("enabled", timeout=TIMEOUT)
   win.Champs.click()  # Close Fields toolbox
-  win.wait("enabled", timeout=30)
+  win.wait("enabled", timeout=TIMEOUT)
   win.Accueil.click_input()  # Check the Home toolbar is selected
-  win.wait("enabled", timeout=30)
+  win.wait("enabled", timeout=TIMEOUT)
 
   # Refresh
-  last_modified = os.path.getmtime(args.excel)
+  last_modified = os.path.getmtime(args.excel)  # Get excel last modification timestamp
   try:
     while True:
-      time.sleep(2)
+      time.sleep(2)  # To avoid high CPU load
+      
       tmp = os.path.getmtime(args.excel)
       if tmp == last_modified:
-        continue
+        continue  # If excel timestamps are the same, the excel file has not been modified. Waiting.
+        
       last_modified = tmp
       logging.info("Refreshing")
       try:
@@ -73,11 +76,10 @@ def main():
         win.Actualiser.Fermer.click_input()  # If errors in data, a popup window needs to be closed
         win.Save.click()  # Save the window
       except Exception as e:
-        logging.error(e)
+        logging.error(e)  # We don't really care, the program must not crash
   except KeyboardInterrupt:
-    pass
+    logging.info("Got stop request, stopping now...")
     
-
   #Close
   logging.info("Exiting")
   win.close()
